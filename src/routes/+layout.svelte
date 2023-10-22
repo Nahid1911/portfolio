4c35cd4b2c51bc9bdf867faa4513409cb9bd27f0
Nahid1911 <nahid.raihan@gmail.com>
2023-10-22 21:06:09 +0600

    Add button to the page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,12 @@
   import "../app.css";
 </script>
 
+<div
+  class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
+>
+  <button>
+    <i class="fa-solid fa-arrow-up" />
+  </button>
+</div>
+
 <slot />
