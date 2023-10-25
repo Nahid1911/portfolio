b78e7e28898cd12bd01e68f04366b3658551d206
Nahid1911 <nahid.raihan@gmail.com>
2023-10-25 20:46:05 +0600

    Delete particles.json

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script>
   import "../app.css";
+  import Header from "../components/Header.svelte";
+  import Footer from "../components/Footer.svelte";
 </script>
 
-<div
-  class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
->
-  <button>
-    <i class="fa-solid fa-arrow-up" />
-  </button>
+<div>
+  <Header />
+  <main>
+    <slot />
+  </main>
+  <Footer />  
 </div>
 
-<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <script>
   import Main from "../components/Main.svelte";
-  import Header from "../components/Header.svelte";
 </script>
-<Header />
 <Main />
