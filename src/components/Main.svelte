1e494690d9a12c1c16557af1a76b5a6bbab9aaa7
Nahid1911 <nahid.raihan@gmail.com>
2023-10-26 07:57:24 +0600

    Add Introduction Section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -8,6 +8,14 @@
         Hi! I'm <span class="poppins text-violet-400">Nahid</span> Raihan <br>
          Full Stack <span class="poppins text-violet-400">Developer</span>
       </h2>
+      <p class="text-base sm:text-lg md:text-xl">My <span class="text-violet-400">favorite tech</span> includes JavaScript, React, Redux, Ruby on Rails, TailwindCSS, BootStrap</p>
+      <a href=" " target="_blank" class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl
+      poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950">
+        <div class="absolute top-0 right-full width-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200" />
+      <h4 class="relative z-9">
+        Get in touch &rarr;
+      </h4>   
+      </a>
     </div>
   </section>
 </main>
