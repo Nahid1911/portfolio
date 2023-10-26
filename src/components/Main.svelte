8e8b6d4d6c383b2f9808f94ae154c650d4128f84
Nahid1911 <nahid.raihan@gmail.com>
2023-10-26 16:47:31 +0600

    Fix alignment

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -2,12 +2,15 @@
 </script>
 
 <main class="flex flex-col flex-1 p-4">
-  <section class="grid grid-cols-1 lg:grid-gols-2 gap-10 py-8 sm:py-8">
+  <section class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-8">
     <div class="flex flex-col lg:justify-center text-center lg-text-left gap-6 md:gap-8 lg:gap-10">
-      <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+      <h2 class="font-semibold text-4xl sm:text-5xl md:text-5xl">
         Hi! I'm <span class="poppins text-violet-400">Nahid</span> Raihan <br>
-         Full Stack <span class="poppins text-violet-400">Developer</span>
+         Full Stack <span class="poppins text-violet-400">Developer</span> with a <span class="poppins text-violet-400">finance</span> background,
       </h2>
+      <p class="text-base sm:text-lg md:text-xl">
+         Unique blend of <span class="text-violet-400">tech skills</span> and <span class="text-violet-400">business acumen</span>, transforming client needs into effective software solutions aligned with business goals.
+        </p>
       <p class="text-base sm:text-lg md:text-xl">My <span class="text-violet-400">favorite tech</span> includes JavaScript, React, Redux, Ruby on Rails, TailwindCSS, BootStrap</p>
       <a href=" " target="_blank" class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl
       poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950">
@@ -16,9 +19,9 @@
         Get in touch &rarr;
       </h4>   
       </a>
-      <div class="relative shadow-2xl grid place-items-center">
-        <img src={"images/Nahid_wearing_panjabi-removebg-preview.jpg"} alt="Profile Image" class="rounded-full object-cover z-[2] max-h-[70vh]">
-      </div>
+    </div>
+    <div class="relative shadow-2xl grid place-items-center">
+      <img src={"images/Nahid_wearing_panjabi-removebg-preview.jpg"} alt="Profile Image" class="rounded-full object-cover z-[2] max-h-[65vh]">
     </div>
   </section>
 </main>
